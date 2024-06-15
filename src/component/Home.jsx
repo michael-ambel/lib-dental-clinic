f60fc4621fbe89f51209d24b3f259c339dbd9f8a
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-06-15 14:35:34 +0300

    Service section with only placeholder done!

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,28 +5,69 @@ import { ReactTyped } from "react-typed";
 const Home = () => {
     return ( 
 
-        <main>
-            <div className='flex items-start justify-start md:justify-between h-screen w-screen pl-6 md:pl-16 pr-6 md:pr-16'>
-                <div className='flex flex-col mt-24 sm:mt-16 w-full lg:w-[50%]'>
-                    <h2 className='font-bold text-[44px] text-gray-500 '>Where healthy smiles begin,</h2>
+        <main className='relative mt-[100px]'>
+            <div className='flex mt-16 sm:mt-0 sm:mb-16 items-start md:items-center justify-start md:justify-between h-auto md:h-[540px] w-screen pl-6 md:pl-16 pr-6 md:pr-16'>
+                <div className='flex flex-col w-full lg:w-[50%]'>
+                    <h2 className='font-bold text-[44px] text-gray-600 '>Where healthy smiles begin,</h2>
                    
                     {/* <ReactTyped className='font-bold text-[40px] text-gray-600' strings={["Where healthy smiles begin,"]} typeSpeed={40} /> */}
-                    <p className='text-[20px] mt-6 text-gray-900'>High quality, compassionate dental care to patients of all ages</p>
+                    <p className='text-[20px] mt-6 text-gray-800'>High quality, compassionate dental care to patients of all ages</p>
                     <div className='flex items-center justify-start mt-5'>
                         <button className='flex items-center justify-center px-6 py-5 mr-5 rounded-[20px] bg-sky-400 text-white h-[46px] font-semibold text-[18px]'>Call</button>
                         <button className='flex items-center justify-center px-7 py-5 rounded-[20px] bg-sky-400 text-white h-[46px] font-semibold text-[18px'>Book an Appointment</button>
                     </div>
-                    <div className='mt-24 sm:mt-10 overflow-hidden'>
-                        <ReactTyped className='font-bold text-[32px] sm:text-[40px] text-gray-600 text-clip ' strings={[ "Cleanings and Exams", "Fillings and Crowns", "Teeth Whitening", "Dental Implants", "Dentures", "Braces and Invisalign", "Pediatric Dentistry" ]} typeSpeed={100} backSpeed={30} attr="" loop>
+                    <div className='mt-10 mb-4 overflow-hidden'>
+                        <ReactTyped className='font-bold text-[32px] sm:text-[40px] text-gray-500 text-clip ' strings={[ "Cleanings and Exams", "Fillings and Crowns", "Teeth Whitening", "Dental Implants", "Dentures", "Braces and Invisalign", "Pediatric Dentistry" ]} typeSpeed={100} backSpeed={30} attr="" loop>
                             <input type="text" />
                         </ReactTyped>
                     </div>
                 </div>
 
-                <div className='py-8 pl-8 hidden h-full lg:block w-[50%]'>
-                    <img className='w-full h-[80%]' src="/stock.jpg" alt="" />
+                <div className='py-10 pl-8 hidden lg:flex items-center justify-center w-[50%]'>
+                    <img className='' src="/lib_dental_005.png" alt="" />
                 </div>
-        </div>
+            </div>
+
+
+
+
+            
+
+            <div className='pb-0 md:pb-[0px] flex-row md:flex items-start justify-start md:justify-between h-auto w-screen pl-6 md:pl-16 pr-6 md:pr-16'>
+                <div className='flex my-[60px] md:my-0 w-full md:w-[46%]'>
+                    <img className='w-full md:w-[80%] rounded-tl-[30px] rounded-[4px]' src="/lib_dental_002.png" alt="" />
+                    <img src="" alt="" />
+                </div>
+                <div className='flex flex-col my-[60px] md:my-0 w-full md:w-[54%]'>
+                    <div className='flex p-3 mb-2 w-[200px] h-[40px] items-center rounded-[30px] text-gray-300 bg-gray-800'>
+                        <h>🕐 24 Hours Available</h>
+                    </div>
+                    <div className='my-2 text-[32px] font-semibold text-gray-600'>
+                        <h2>The Best Dental Clinic That You Can Trust</h2>
+                    </div>
+                    <div>
+                        <div className='my-2 text-[20px]'>
+                            <div className='my-4'>
+                                <p>Smile brighter than your future! Level up your smile game! We're dental dreamweavers, using advanced technology to craft exceptional care.</p>
+                            </div>
+                            
+                            <div className='my-4 grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-2 font-medium '>
+                                <div className='grid grid-rows-2 gap-1'>
+                                    <p className='flex items-center mb-2 ml-1'> <span className='flex justify-center items-center pb-1 w-[26px] h-[26px] rounded-[20px] text-gray- border-[1px] border-sky-500 mr-[5px]'>✔</span>  100% Expert Doctors</p>
+                                    <p className='flex items-center mb-2 ml-1'> <span className='flex justify-center items-center pb-1 w-[26px] h-[26px] rounded-[20px] text-gray- border-[1px] border-sky-500 mr-[5px]'>✔</span>  Posetive Feedback</p>
+                                </div>
+                                <div className='grid grid-rows-2 gap-1'>
+                                    <p className='flex items-center mb-2 ml-1'> <span className='flex justify-center items-center pb-1 w-[26px] h-[26px] rounded-[20px] text-gray- border-[1px] border-sky-500 mr-[5px]'>✔</span>  Advanced Instrument</p>
+                                    <p className='flex items-center mb-2 ml-1'> <span className='flex justify-center items-center pb-1 w-[26px] h-[26px] rounded-[20px] text-gray- border-[1px] border-sky-500 mr-[5px]'>✔</span>  Fair Price</p>
+                                </div>
+                            </div>
+                            <button className='flex items-center px-[38px] py-[24px] my-5 ml-1 h-[40px] text-white font-bold bg-sky-400 rounded-[20px]'>Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
 
         </main>
      );
---
+++ src/component/Services.jsx
@@ -0,0 +1,85 @@
+const Services = () => {
+    return ( 
+        <div className='grid grid-row-2 my-[60px] w-screen pl-6 md:pl-16 pr-6 md:pr-16'>
+            <div className="text-[36px] my-8 font-bold text-gray-500">
+                <h2>Our Services</h2>
+            </div>
+            <div className="grid grid-cols-4 gap-4 h-[700px]">
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+                <div className=" h-[200px]">
+                    <div className="">
+                        <img className="w-[280px] rounded-md" src="/lib_dental_003.png" alt="" />
+                    </div>
+                    <div className="flex items-center justify-center w-[200px] relative left-[40px] bottom-[20px] text-[20] font-bold text-sky-500 rounded-[20px] p-[8px] bg-white">
+                        <h2 className="">Cleanings and Exams</h2>
+                    </div>
+                    <p className="relative bottom-[20px] text-gray-500">Bi-annual dental defense: Plaque removal, cavity patrol.</p>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Services;
